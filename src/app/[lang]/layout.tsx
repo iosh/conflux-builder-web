db8586433164f85b215db3dbb8e9e163f0342bc1
iosh <ioshx@foxmail.com>
2025-08-23 09:54:59 +0800

    op

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../globals.css";
 import { i18n } from "@/i18n-config";
 import Providers from "@/components/providers";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -25,7 +22,7 @@ export default async function RootLayout({
   const { lang } = await params;
   return (
     <html lang={lang}>
-      <body className={inter.className}>
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
